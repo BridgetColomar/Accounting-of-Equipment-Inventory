924a40e8dfc9cf3d9167f1b07e87e05bbe1e1dae
Мария <abrabridget@yandex.ru>
2025-05-06 23:43:27 +0300

    specification context

--- AccountingOfEquipmentInventoryManagementDbContext/Context/Connections/SqliteDbContext.cs
+++ AccountingOfEquipmentInventoryManagementDbContext/Context/Connections/SqliteDbContext.cs
@@ -11,18 +11,29 @@ namespace AccountingOfEquipmentInventoryManagementDbContext.Context.Connections
 {
     public class SqliteDbContext : AppDbContext
     {
+        // Абсолютный путь к файлу базы данных 
+        private const string AbsoluteDbPath = @"D:\C#\Accounting-of-Equipment-Inventory\AccountingOfEquipmentInventoryManagementDbContext\EquipmentInventory.db";
+        // Строка подключения, указывающая на этот файл
+        private readonly string _connectionString = $"Data Source={AbsoluteDbPath}";
         public SqliteDbContext(DbContextOptions<SqliteDbContext> options)
             : base(options)   // передаём опции в базовый конструктор
         {
+            // Если файл базы данных не существует, создаем его
+            if (!File.Exists(AbsoluteDbPath))
+            {
+                Database.EnsureCreated();
+                Debug.WriteLine($"{this.GetType().Name} was created at {AbsoluteDbPath}.");
+            }
+            else
+            {
+                Debug.WriteLine($"{this.GetType().Name} connected to {AbsoluteDbPath}.");
+            }
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                // Использование SQLite в качестве базы данных
-                optionsBuilder.UseSqlite("Data Source=EquipmentInventory.db");
-            }
+            // Используем SQLite с заданной строкой подключения
+            optionsBuilder.UseSqlite(_connectionString);
             base.OnConfiguring(optionsBuilder);
         }
     }
